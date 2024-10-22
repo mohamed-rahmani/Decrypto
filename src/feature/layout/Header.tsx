f797de351dd0eec0613706ecdeac29c8b98af192
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-10-22 20:31:19 +0200

    [ADDED] Mobile Nav Menu

--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -1,31 +1,67 @@
-import { ThemeToggle } from "@/src/theme/ThemeToggle";
-import { LockKeyhole } from "lucide-react";
-import Link from "next/link";
+"use client";
 
-export const Header = async () => {
+import { ThemeToggle } from "@/src/theme/ThemeToggle";
+import { LockKeyhole, Menu, X } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="flex items-center justify-between p-4 relative">
+    <header className="flex items-center justify-between p-4 relative z-50">
       <h2 className="text-2xl font-bold">
         <Link href="/" className="flex items-center">
           <LockKeyhole className="text-green-400" size={30} />
           <p className="text-green-400 pl-1">De</p>crypto
         </Link>
       </h2>
-      <nav className="absolute left-1/2 transform -translate-x-1/2">
-        <ul className="flex space-x-8 font-bold">
-          <li>
-            <Link href="/RSA">RSA</Link>
-          </li>
-          <li>
-            <Link href="/Affine">Affine</Link>
-          </li>
-          <li>
-            <Link href="/Cesar">César</Link>
-          </li>
-        </ul>
+      <nav className="hidden md:flex space-x-8 font-bold">
+        <Link href="/RSA">RSA</Link>
+        <Link href="/Affine">Affine</Link>
+        <Link href="/Cesar">César</Link>
       </nav>
-      <div className="flex">
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex flex-col justify-center items-center z-40">
+          <button
+            onClick={toggleMenu}
+            aria-label="Close Menu"
+            className="absolute top-6 right-6 text-white"
+          >
+            <X size={30} />
+          </button>
+          <nav className="text-center">
+            <ul className="space-y-8">
+              <li>
+                <Link href="/RSA" onClick={() => setIsMenuOpen(false)}>
+                  RSA
+                </Link>
+              </li>
+              <li>
+                <Link href="/Affine" onClick={() => setIsMenuOpen(false)}>
+                  Affine
+                </Link>
+              </li>
+              <li>
+                <Link href="/Cesar" onClick={() => setIsMenuOpen(false)}>
+                  César
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+      <div className="hidden md:block">
         <ThemeToggle />
+      </div>
+      <div className="md:hidden">
+        <button onClick={toggleMenu} aria-label="Toggle Menu">
+          {!isMenuOpen && <Menu size={30} />}
+        </button>
       </div>
     </header>
   );
