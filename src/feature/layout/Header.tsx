05057d04e277675fe7e8a49839c34e1daf1bcdb5
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-10-22 22:14:45 +0200

    [ADDED] Hover Effect on Nav Link

--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -13,18 +13,50 @@ export const Header = () => {
   };
 
   return (
-    <header className="flex items-center justify-between p-4 relative z-50">
+    <header className="flex items-center justify-between p-4 relative">
       <h2 className="text-2xl font-bold">
         <Link href="/" className="flex items-center">
           <LockKeyhole className="text-green-400" size={30} />
           <p className="text-green-400 pl-1">De</p>crypto
         </Link>
       </h2>
-      <nav className="hidden md:flex space-x-8 font-bold">
-        <Link href="/RSA">RSA</Link>
-        <Link href="/Affine">Affine</Link>
-        <Link href="/Cesar">César</Link>
+
+      <nav className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
+        <ul className="flex space-x-8 font-bold">
+          <li>
+            <Link
+              href="/RSA"
+              className="hover:text-green-400 transition-colors duration-300 transform hover:scale-105"
+            >
+              RSA
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/Affine"
+              className="hover:text-green-400 transition-colors duration-300 transform hover:scale-105"
+            >
+              Affine
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/Cesar"
+              className="hover:text-green-400 transition-colors duration-300 transform hover:scale-105"
+            >
+              César
+            </Link>
+          </li>
+        </ul>
       </nav>
+
+      <div className="md:hidden flex items-center space-x-4">
+        <ThemeToggle />
+        <button onClick={toggleMenu} aria-label="Toggle Menu">
+          {isMenuOpen ? <X size={30} /> : <Menu size={30} />}
+        </button>
+      </div>
+
       {isMenuOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex flex-col justify-center items-center z-40">
           <button
@@ -35,7 +67,7 @@ export const Header = () => {
             <X size={30} />
           </button>
           <nav className="text-center">
-            <ul className="space-y-8">
+            <ul className="space-y-8 text-2xl font-bold">
               <li>
                 <Link href="/RSA" onClick={() => setIsMenuOpen(false)}>
                   RSA
@@ -55,13 +87,9 @@ export const Header = () => {
           </nav>
         </div>
       )}
+
       <div className="hidden md:block">
         <ThemeToggle />
-      </div>
-      <div className="md:hidden">
-        <button onClick={toggleMenu} aria-label="Toggle Menu">
-          {!isMenuOpen && <Menu size={30} />}
-        </button>
       </div>
     </header>
   );
